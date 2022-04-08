bea9b44780ee0477b110404fc564b15b5c1a16f8
Alvaro-Mora17 <77671080+Alvaro-Mora17@users.noreply.github.com>
2022-04-08 14:13:32 +0200

    Add files via upload

---
+++ Moracorp/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Aqui indicamos que se cargara el fichero "models.py"
+# Si creamos mas modelos, deben importarse en este fichero
+from . import productos
+from . import ventas
+from . import compras
+from . import proveedores
+from . import clientes
+from . import pedidos
+from . import transaccion_venta
---
+++ Moracorp/static/src/js/import.js
@@ -0,0 +1,58 @@
+odoo.define('MORACORP.import', function (require)
+
+{
+   "use strict";
+   var core = require('web.core');
+
+   var ListController = require('web.ListController');
+
+   ListController.include({
+
+       renderButtons: function($node)
+
+       {
+
+           this._super.apply(this, arguments);
+
+               if (this.$buttons)
+
+               {
+
+                   let import_button = this.$buttons.find('.import_data_button');
+
+                   import_button && import_button.click(this.proxy('import_button')) ;
+
+               }
+
+       },
+
+
+       import_button: function ()
+
+       {
+
+            this.do_action({
+
+               type: "ir.actions.act_window",
+
+               name: "import",
+
+               res_model: "moracorp.productos",
+
+               views: [[false,'form']],
+
+               target: 'new',
+
+               view_mode : 'form',
+
+               flags: {'form': {'action_buttons': true, 'options': {'mode': 'edit'}}}
+
+               });
+
+           return { 'type': 'ir.actions.client','tag': 'reload', }
+
+       }
+
+   });
+
+})
